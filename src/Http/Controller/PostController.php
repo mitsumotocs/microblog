d65985313c853f4c4b006148b6c046841ad03bc8
Joji Mitsumoto <mitsumotocs@gmail.com>
2023-11-29 16:17:34 +0900

    Updated

--- src/Http/Controller/PostController.php
+++ src/Http/Controller/PostController.php
@@ -17,8 +17,31 @@ class PostController implements ControllerInterface
 
     public function index(Request $request): Response
     {
+        // read post index
+        $indexFile = __DIR__ . '/../../../storage/meta/index.json';
+        $indexDataString = file_get_contents($indexFile);
+        if ($indexDataString === false) {
+            throw new RuntimeException(sprintf('Unable to get %s', $indexDataString));
+        }
+        $indexData = json_decode($indexDataString, true);
+
+        // read posts
+        $posts = [];
+        foreach ($indexData as $postId) {
+            $postFile = __DIR__ . '/../../../storage/posts/' . $postId . '.json';
+            $postDataString = file_get_contents($postFile);
+            if ($postDataString === false) {
+                throw new RuntimeException(sprintf('Unable to get %s', $postDataString));
+            }
+            $postData = json_decode($postDataString, true);
+            $posts[] = $postData;
+        }
+
         $response = new Response();
         $response->body .= '<html><body>';
+        $response->body .= '<pre>';
+        $response->body .= var_export($posts, true);
+        $response->body .= '</pre>';
         $response->body .= '</body><html>';
 
         return $response;
@@ -26,6 +49,7 @@ class PostController implements ControllerInterface
 
     public function create(Request $request): Response
     {
+        // save the post
         $content = trim($request->inputs['content'] ?? '');
 
         $id = (string) Uuid::uuid4();
@@ -34,9 +58,21 @@ class PostController implements ControllerInterface
 
         $postData = compact('id', 'createdAt', 'content', 'request');
 
-        $file = __DIR__ . '/../../../storage/posts/' . $id . '.json';
-        if (file_put_contents($file, json_encode($postData), LOCK_EX) === false) {
-            throw new RuntimeException(sprintf('Unable to put %s', $file));
+        $postFile = __DIR__ . '/../../../storage/posts/' . $id . '.json';
+        if (file_put_contents($postFile, json_encode($postData), LOCK_EX) === false) {
+            throw new RuntimeException(sprintf('Unable to put %s', $postFile));
+        }
+
+        // update post index
+        $indexFile = __DIR__ . '/../../../storage/meta/index.json';
+        $indexDataString = file_get_contents($indexFile);
+        if ($indexDataString === false) {
+            throw new RuntimeException(sprintf('Unable to get %s', $indexDataString));
+        }
+        $indexData = json_decode($indexDataString, true);
+        array_push($indexData, $postData['id']);
+        if (file_put_contents($indexFile, json_encode($indexData), LOCK_EX) == false) {
+            throw new RuntimeException(sprintf('Unable to put %s', $indexFile));
         }
 
         $response = new Response();
